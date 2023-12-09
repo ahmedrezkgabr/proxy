d7314bff692054bfa79e5607ae683e5c9cebfe6c
rezk <ahmedrezkofficial@gmail.com>
2023-12-09 16:22:41 +0200

    implement 'Proxy_Error_t Proxy::setConfigFilePath(std::string)'

--- proxy/proxy.cpp
+++ proxy/proxy.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
+#include "boost/property_tree/ini_parser.hpp"
 #include "mqtt/async_client.h"
 #include "proxy.hpp"
+
+/**
+ * @brief Sets the configuration File Path.
+ *
+ * @param filePath as string
+ * @return An enum of Proxy_Error_t.
+ */
+Proxy_Error_t Proxy::setConfigFilePath(std::string filePath)
+{
+    this->configFilePath = filePath;
+    return Proxy_Error_t::OK;
+}
