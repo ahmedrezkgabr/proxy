529cf6f11e1c1018dd96ff67505b7da006e10304
ahmedelmola224 <ahmedelmola224@gmail.com>
2023-12-10 16:54:10 +0200

    editing 'rpi.hpp'

--- rpi/rpi.hpp
+++ rpi/rpi.hpp
@@ -1,33 +1,17 @@
-#ifndef RPI__HPP_
-#define RPI__HPP_
-
-#include <iostream>
+#ifndef RPI_HPP
+#define RPI_HPP
 #include "mqtt/async_client.h"
-
-enum En_Flag_t
+class MyCallBack : public virtual mqtt::callback
 {
-    CARLA,
-    RPIS,
-    ACTIONS,
-    SENSORS
-};
-
-class Rpi : private mqtt::async_client
-{
-private:
-    /* data */
 public:
-    Rpi();
-    ~Rpi();
-    void setConfigFilePath(std::string);
-    bool loadConfiguaration();
-    void loadDefaultConfiguaration();
-    std::string getConfigFilePath();
-    mqtt::token_ptr subscribe(void);
-    mqtt::delivery_token_ptr publish();
-    uint8_t getRxFalg();
-    void getData();
-    void logData();
+    int recived_msg_flag = 0;
+    std::string msg;
+    uint64_t counter_rec = 0;
+    uint64_t counter_sen = 0;
+    /* overriding the functions on every action may happen */
+    void connection_lost(const std::string &cause) override;         /* call back function called when lossing the connection with the broker */
+    void message_arrived(mqtt::const_message_ptr msg) override;      /* call back function called when receiving a message */
+    void delivery_complete(mqtt::delivery_token_ptr token) override; /* call back function called when delivery complete */
 };
 
-#endif /* RPI__HPP_ */
+#endif
