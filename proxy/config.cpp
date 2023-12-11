4c3684fb8e84acac1157e142b3d739cfc7ceca4a
rezk <ahmedrezkofficial@gmail.com>
2023-12-11 21:38:41 +0200

    modify to suite the upgrate at hpp file

--- proxy/config.cpp
+++ proxy/config.cpp
@@ -6,8 +6,8 @@ ConfigHandler::ConfigHandler(/* args */) {}
 ConfigHandler::~ConfigHandler()
 {
     /* destroy the vectors */
-    this->pubTopicsNames.~vector();
-    this->subTopicsNames.~vector();
+    this->myTopicsData.pubTopicsNames.~vector();
+    this->myTopicsData.subTopicsNames.~vector();
 }
 
 /**
@@ -50,27 +50,28 @@ Config_Error_t ConfigHandler::loadConfiguaration(void)
     boost::property_tree::ini_parser::read_ini(this->configFilePath, configTree);
 
     /* get the client parameters */
-    this->address = configTree.get<std::string>("client.address");
-    this->clientId = configTree.get<std::string>("client.clientId");
-    this->maxBufMsgs = configTree.get<uint32_t>("client.maxBufMsgs");
-    this->cleanSession = configTree.get<bool>("client.cleanSession");
-    this->autoReconnect = configTree.get<bool>("client.autoReconnect");
-    this->keepAliveTime = configTree.get<uint64_t>("client.keepAliveTime");
-    this->qualityOfService = configTree.get<uint8_t>("client.qualityOfService");
-    this->retainedFlag = configTree.get<bool>("client.retainedFlag");
+    this->myClientData.address = configTree.get<std::string>("client.address");
+    this->myClientData.clientId = configTree.get<std::string>("client.clientId");
+    this->myClientData.maxBufMsgs = configTree.get<uint32_t>("client.maxBufMsgs");
+    this->myClientData.cleanSession = configTree.get<bool>("client.cleanSession");
+    this->myClientData.autoReconnect = configTree.get<bool>("client.autoReconnect");
+    this->myClientData.keepAliveTime = configTree.get<uint64_t>("client.keepAliveTime");
+    
+    this->myTopicsData.qualityOfService = configTree.get<uint8_t>("topics.qualityOfService");
+    this->myTopicsData.retainedFlag = configTree.get<bool>("topics.retainedFlag");
 
     /* get the number of the rpis in the system */
-    this->numberOfRpis = configTree.get<uint8_t>("topics.numberOfRpis");
+    this->myTopicsData.numberOfRpis = configTree.get<uint8_t>("topics.numberOfRpis");
 
     /* get carla's topics */
-    this->pubTopicsNames.push_back(configTree.get<std::string>("topics.carlaActionsTopic"));
-    this->subTopicsNames.push_back(configTree.get<std::string>("topics.carlaSensorsTopic"));
+    this->myTopicsData.pubTopicsNames.push_back(configTree.get<std::string>("topics.carlaActionsTopic"));
+    this->myTopicsData.subTopicsNames.push_back(configTree.get<std::string>("topics.carlaSensorsTopic"));
 
     /* loop to get rpix topics : x = [1:numberOfRpis] */
-    for (uint8_t i = 0; i < this->numberOfRpis; i++)
+    for (uint8_t i = 0; i < this->myTopicsData.numberOfRpis; i++)
     {
-        this->pubTopicsNames.push_back(configTree.get<std::string>("topics.rpi0" + std::to_string(i + 1) + "SensorsTopic"));
-        this->subTopicsNames.push_back(configTree.get<std::string>("topics.rpi0" + std::to_string(i + 1) + "ActionsTopic"));
+        this->myTopicsData.pubTopicsNames.push_back(configTree.get<std::string>("topics.rpi0" + std::to_string(i + 1) + "SensorsTopic"));
+        this->myTopicsData.subTopicsNames.push_back(configTree.get<std::string>("topics.rpi0" + std::to_string(i + 1) + "ActionsTopic"));
     }
 
     return Config_Error_t::OK;
