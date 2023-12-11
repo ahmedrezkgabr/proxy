a0c87db0ee9564160cd4ede43bea61fbecaa7c71
rezk <ahmedrezkofficial@gmail.com>
2023-12-11 21:19:55 +0200

    making all data fields private

--- proxy/config.hpp
+++ proxy/config.hpp
@@ -10,29 +10,11 @@ enum class Config_Error_t
     NOT_OK
 };
 
-
 /* data */
-
 
 class ConfigHandler
 {
 public:
-    /* client */
-    std::string address;
-    std::string clientId;
-    uint32_t maxBufMsgs;
-    bool cleanSession;
-    bool autoReconnect;
-    uint64_t keepAliveTime;
-
-    /* topics */
-    std::vector<std::string> pubTopicsNames;
-    std::vector<std::string> subTopicsNames;
-    uint8_t numberOfRpis;
-    uint8_t qualityOfService;
-    bool retainedFlag;
-
-    std::string configFilePath{"./../config.ini"};
     ConfigHandler(/* args */);
     ~ConfigHandler();
 
@@ -60,6 +42,24 @@ public:
      * @return An enum of Proxy_Error_t.
      */
     Config_Error_t loadConfiguaration(void);
+
+private:
+    /* client */
+    std::string address;
+    std::string clientId;
+    uint32_t maxBufMsgs;
+    bool cleanSession;
+    bool autoReconnect;
+    uint64_t keepAliveTime;
+
+    /* topics */
+    std::vector<std::string> pubTopicsNames;
+    std::vector<std::string> subTopicsNames;
+    uint8_t numberOfRpis;
+    uint8_t qualityOfService;
+    bool retainedFlag;
+
+    std::string configFilePath{"./../config.ini"};
 };
 
 #endif /* CONFIG__HHP_ */
