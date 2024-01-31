1fc5f2287da35993ce3301844fb6fb1fdf72163f
rezk <ahmedrezkofficial@gmail.com>
2024-01-31 16:05:00 +0200

    formate the document

--- proxy/unitTest/configTest/configTest_testCases.cpp
+++ proxy/unitTest/configTest/configTest_testCases.cpp
@@ -1,39 +1,42 @@
 #include "configTest_testCases.hpp"
 
-void configTest::SetUp()
-{
-}
-void configTest::TearDown()
-{
-}
+void configTest::SetUp() {}
+void configTest::TearDown() {}
 
 TEST_F(configTest, getConfigFilePath)
 {
+    /* arrange */
     std::string path{"../../TestPath"};
-    // actions
-    handler.setConfigFilePath(path);
-    // expect
 
+    /* act */
+    handler.setConfigFilePath(path);
+
+    /* expect */
     EXPECT_STREQ(path.c_str(), handler.getConfigFilePath().c_str());
 }
 
 TEST_F(configTest, loadConfiguaration)
 {
+    /* arrange */
     std::string path{"./../../../config.ini"};
-    // actions
+
+    /* act */
     handler.setConfigFilePath(path);
-    // expect
+
+    /* expect */
     EXPECT_EQ(Config_Error_t::OK, handler.loadConfiguaration());
 }
 
 TEST_F(configTest, getAddress)
 {
+    /* arrange */
     std::string address{"mqtt://localhost:1883"};
-    // actions
     std::string path{"./../../../config.ini"};
-    // actions
+
+    /* act */
     handler.setConfigFilePath(path);
     handler.loadConfiguaration();
-    // expect
+
+    /* expect */
     EXPECT_STREQ(address.c_str(), handler.getAddress().c_str());
 }
