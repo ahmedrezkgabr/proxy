998b6682b885eb1e3fb89a8804a789f0466e6cb0
rezk <ahmedrezkofficial@gmail.com>
2024-06-24 10:14:10 +0300

    rename the application that acts like the target from 'rpi' to 'trgt'

---
+++ trgt/build/CMakeFiles/trgt.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rezk/disktop/proxy/rpi/filehandling.cpp" "CMakeFiles/trgt.dir/filehandling.cpp.o" "gcc" "CMakeFiles/trgt.dir/filehandling.cpp.o.d"
+  "/home/rezk/disktop/proxy/rpi/main.cpp" "CMakeFiles/trgt.dir/main.cpp.o" "gcc" "CMakeFiles/trgt.dir/main.cpp.o.d"
+  "/home/rezk/disktop/proxy/rpi/trgt.cpp" "CMakeFiles/trgt.dir/trgt.cpp.o" "gcc" "CMakeFiles/trgt.dir/trgt.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ trgt/build/CMakeFiles/trgt.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trgt.dir/filehandling.cpp.o"
+  "CMakeFiles/trgt.dir/filehandling.cpp.o.d"
+  "CMakeFiles/trgt.dir/main.cpp.o"
+  "CMakeFiles/trgt.dir/main.cpp.o.d"
+  "CMakeFiles/trgt.dir/trgt.cpp.o"
+  "CMakeFiles/trgt.dir/trgt.cpp.o.d"
+  "trgt"
+  "trgt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trgt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
